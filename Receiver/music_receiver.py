506104aec1a13db290db2e1053bd158ad729ee47
guillaumeShare <leignier@ece.fr>
2015-07-30 11:58:12 +0200

    Update music_receiver.py
    Choosing file enabled (last commit was an error)

--- Receiver/music_receiver.py
+++ Receiver/music_receiver.py
@@ -1,45 +1,115 @@
+__author__ = 'USER'
+#! /usr/bin/env python
+# encoding: utf-8
+
+# Copyright Steinwurf ApS 2015.
+# Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+# See accompanying file LICENSE.rst or
+# http://www.steinwurf.com/licensing
+
+import argparse
+import kodo
+import socket
+import struct
+import sys
+import time
+import numpy as np
+import cv2
+import pymedia
+import pymedia.audio.sound as sound
+import wave
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
 
 
-def split_file(file, prefix, max_size, buffer=1024):
+def main():
     """
-    file: the input file
-    prefix: prefix of the output files that will be created
-    max_size: maximum size of each created file in bytes
-    buffer: buffer size in bytes
+    Multicast example, reciever part.
 
-    Returns the number of parts created.
+    An example where data is received, decoded, and finally written to a file.
     """
-    with open(file, 'r+b') as src:
-        suffix = 0
-        while True:
-            with open(prefix + '.%s' % suffix, 'w+b') as tgt:
-                written = 0
-                while written < max_size:
-                    data = src.read(buffer)
-                    if data:
-                        tgt.write(data)
-                        written += buffer
-                    else:
-                        return suffix
-                suffix += 1
+    parser = argparse.ArgumentParser(description=main.__doc__)
 
+    parser.add_argument(
+        '--ip',
+        type=str,
+        help='The ip to send to.',
+        default=MCAST_GRP)
 
-def cat_files(infiles, outfile, buffer=1024):
-    """
-    infiles: a list of files
-    outfile: the file that will be created
-    buffer: buffer size in bytes
-    """
-    with open(outfile, 'w+b') as tgt:
-        for infile in sorted(infiles):
-            with open(infile, 'r+b') as src:
-                while True:
-                    data = src.read(buffer)
-                    if data:
-                        tgt.write(data)
-                    else:
-                        break
+    parser.add_argument(
+        '--port',
+        type=int,
+        help='The port to send to.',
+        default=MCAST_PORT)
 
-split_file('Kalimba.wav', 'Kalimba_split', 10*1024*1024)
-list = ['Kalimba_split.0', 'Kalimba_split.1', 'Kalimba_split.2', 'Kalimba_split.3', 'Kalimba_split.4', 'Kalimba_split.5']
-cat_files(list, 'Kalimba_New.wav')
+    parser.add_argument(
+        '--dry-run',
+        action='store_true',
+        help='Run without network use.')
+
+    args = parser.parse_args()
+
+    sock = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_DGRAM,
+        proto=socket.IPPROTO_UDP)
+
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    sock.bind(('', args.port))
+    mreq = struct.pack("4sl", socket.inet_aton(args.ip), socket.INADDR_ANY)
+
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    # Set the number of symbols (i.e. the generation size in RLNC
+    # terminology) and the size of a symbol in bytes
+    symbols = int(sock.recv(100))
+    print symbols
+    symbol_size = int(sock.recv(100))
+    print symbol_size
+
+    # Get the filename from the sender
+    file_name = sock.recv(100)
+    print file_name
+    sampleRate = int(sock.recv(100))
+    # In the following we will make an decoder factory.
+    # The factories are used to build actual decoder
+    decoder_factory = kodo.FullVectorDecoderFactoryBinary(
+        max_symbols=symbols,
+        max_symbol_size=symbol_size)
+
+    decoder = decoder_factory.build()
+
+    print "SampleRate {}".format(sampleRate)
+    snd1= sound.Output(sampleRate, 1, sound.AFMT_S16_LE )
+
+    rating = int(sampleRate*1.02)
+    if args.dry_run:
+        sys.exit(0)
+
+    print("Processing...")
+    i = 0
+    while not decoder.is_complete():
+        time.sleep(0.2)
+        packet = sock.recv(rating)
+
+        decoder.read_payload(packet)
+        snd1.play(decoder.copy_symbols()[i*rating:i*rating + rating])
+        #print len(decoder.copy_symbols())
+        #snd1.play(decoder.copy_symbols())
+       #while snd1.isPlaying():
+            # time.sleep(0.05)
+
+        print("Packet received!")
+        print("Decoder rank: {}/{}".format(decoder.rank(), decoder.symbols()))
+        i += 1
+
+    # Write the decoded data to the output file
+    f = open(file_name, 'wb')
+    f.write(decoder.copy_symbols())
+    f.close()
+    print("Processing finished.")
+
+if __name__ == "__main__":
+    main()
